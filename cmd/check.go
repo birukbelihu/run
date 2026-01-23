a759b72864b1a975ba3cfb1a52e1ec37ad54e91d
birukbelihu <birukbelihu1995@gmail.com>
2026-01-23 21:16:17 +0300

    Removed cli arg restriction to allow batch runtime check

--- cmd/check.go
+++ cmd/check.go
@@ -10,22 +10,12 @@ var checkCmd = &cobra.Command{
 	Use:   "check",
 	Short: CheckCmdShortDescription,
 	Long:  CheckCmdLongDescription,
-	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("check called")
+		fmt.Println(len(args))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(checkCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// checkCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// checkCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
